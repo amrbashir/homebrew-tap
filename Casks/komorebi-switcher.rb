bef8613d8191740a24e064e070215269eabdde59
Amr Bashir <github@amrbashir.me>
2026-01-03 18:59:51 +0200

    Fix description formatting in komorebi-switcher.rb

--- Casks/komorebi-switcher.rb
+++ Casks/komorebi-switcher.rb
@@ -5,7 +5,7 @@ cask "komorebi-switcher" do
   url "https://github.com/amrbashir/komorebi-switcher/releases/download/v#{version}/komorebi-switcher.dmg",
       verified: "github.com/amrbashir/komorebi-switcher/"
   name "komorebi-switcher"
-  desc "Minimal switcher for Komorebi tiling manager, integrated into the menu bar."
+  desc "Minimal switcher for Komorebi tiling manager, integrated into the menu bar"
   homepage "https://github.com/amrbashir/komorebi-switcher"
 
   livecheck do
@@ -14,7 +14,6 @@ cask "komorebi-switcher" do
   end
 
   app "komorebi-switcher.app"
-
   binary "#{appdir}/komorebi-switcher.app/Contents/MacOS/komorebi-switcher"
 
   postflight do
