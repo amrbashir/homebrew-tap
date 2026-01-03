436b3c628c12767b698d5368bad01af6fc4eab50
Amr Bashir <github@amrbashir.me>
2026-01-03 17:43:55 +0200

    Fix sha256 hash format in komorebi-switcher.rb

--- Casks/komorebi-switcher.rb
+++ Casks/komorebi-switcher.rb
@@ -1,6 +1,6 @@
 cask "komorebi-switcher" do
   version "0.9.0"
-  sha256 "sha256:abb592aa0a8c55f86c5ae3e9bd9e92a7a724c53a339fe4f480793773881dea38"
+  sha256 "abb592aa0a8c55f86c5ae3e9bd9e92a7a724c53a339fe4f480793773881dea38"
 
   url "https://github.com/amrbashir/komorebi-switcher/releases/download/v#{version}/komorebi-switcher.dmg",
       verified: "github.com/amrbashir/komorebi-switcher/"
